7e2cd8f444c3495a50283991b65a1adbe34baea3
Alessio Macrì <m.alessio02@gmail.com>
2025-06-18 18:14:04 +0200

    upload aggiungi carta

--- GestionaleCarte/Program.cs
+++ GestionaleCarte/Program.cs
@@ -8,12 +8,20 @@ public class Tutorial1
 {
     public static void Main()
     {
-        string connStr = "server=localhost;user=root;database=GestionaleCarte;port=3306;password=1234";
+        string connStr = "server=localhost;user=root;database=GestionaleCarte;port=3306;password=";
+        Console.Write($"Inserisci la password del database: ");
+        string p = Console.ReadLine();
+        connStr += p;
         MySqlConnection conn = new MySqlConnection(connStr);
         try
         {
             Console.WriteLine("Connecting to MySQL...");
             conn.Open();
+            // 
+            Carta a = new Carta();
+            a.AggiungiCarta(conn);
+
+
         }
         catch (Exception ex)
         {
