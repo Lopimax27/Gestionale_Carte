90ee6e4f354d505b6f299763e19acfaa810a8887
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-06-18 12:18:43 +0200

    ORG

---
+++ GestionaleCarte/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+public class Tutorial1
+{
+    public static void Main()
+    {
+        string connStr = "server=localhost;user=root;database=GestionaleCarte;port=3306;password=1234";
+        MySqlConnection conn = new MySqlConnection(connStr);
+        try
+        {
+            Console.WriteLine("Connecting to MySQL...");
+            conn.Open();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        conn.Close();
+        Console.WriteLine("Done.");
+    }
+}
