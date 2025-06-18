57b8da91bbde8743bdbaf69c8516af1e0922ba7d
Alessandro Lopardo <alessandrolopardo.26@outlook.it>
2025-06-18 12:55:33 +0200

    update

--- GestionaleCarte.sql
+++ GestionaleCarte.sql
@@ -1,49 +1,48 @@
 create database GestionaleCarte;
 use GestionaleCarte;
 
-CREATE TABLE utente(
+CREATE TABLE Utente(
 id_utente INT PRIMARY KEY AUTO_INCREMENT,
 username varchar(100) UNIQUE NOT NULL,
 email varchar(100) UNIQUE NOT NULL,
-password varchar(100) UNIQUE NOT NULL,
-is_admin BOOLEAN DEFAULT FALSE
+password_hash varchar(100) NOT NULL,
+is_admin BOOL DEFAULT FALSE
 );
 
-CREATE TABLE collezione(
+CREATE TABLE Collezione(
 id_collezione INT PRIMARY KEY AUTO_INCREMENT,
-nome_collezione varchar(100) UNIQUE NOT NULL,
-id_utente INT,
+nome_collezione varchar(100) NOT NULL,
+id_utente INT unique,
 id_carta INT,
 FOREIGN KEY (id_utente) REFERENCES utente(id_utente),
 FOREIGN KEY (id_carta) REFERENCES carta(id_carta)
 );
 
-CREATE TABLE carta(
+CREATE TABLE Carta(
 id_carta INT PRIMARY KEY AUTO_INCREMENT,
-nome_pokemon varchar(100) UNIQUE NOT NULL,
-tipo ENUM("Acqua", "Fuoco", "Erba", "Lampo", "Psico", "Lotta", "Oscurità", "Metallo", "Normale", "Folletto", "Drago"),
-rarita ENUM("Comune", "Non Comune" , "Rara", "Rara-Holo", "Ultra-Rara", "Secret-Rare"),
+nome_pokemon varchar(100) NOT NULL,
+tipo ENUM("Acqua", "Fuoco", "Erba", "Elettro", "Psico", "Lotta", "Buio", "Metallo", "Normale", "Folletto", "Drago"),
+rarita ENUM("Comune", "Non Comune" , "Rara", "Rara-Holo", "Ultra-Rara", "Rara-Segreta"),
 prezzo DECIMAL (10,2),
 url_img varchar(255),
-is_reverse BOOLEAN DEFAULT FALSE,
+is_reverse BOOL DEFAULT FALSE,
 id_espansione INT,
-id_collezione INT,
-FOREIGN KEY (id_espansione) REFERENCES espansione(id_espansione),
-FOREIGN KEY (id_collezione) REFERENCES collezione(id_collezione)
+FOREIGN KEY (id_espansione) REFERENCES espansione(id_espansione)
 );
 
-CREATE TABLE espansione(
+CREATE TABLE Espansione(
 id_espansione INT PRIMARY KEY AUTO_INCREMENT,
 nome_espansione varchar(100) UNIQUE NOT NULL,
 anno_espansione DATE
 );
 
-CREATE TABLE album(
+CREATE TABLE Album(
 id_album INT PRIMARY KEY AUTO_INCREMENT,
+nome_album varchar(100) not null,
 id_collezione INT,
 id_carta INT,
-is_obtained BOOLEAN DEFAULT FALSE,
-is_wanted BOOLEAN DEFAULT FALSE,
+is_obtained BOOL DEFAULT FALSE,
+is_wanted BOOL DEFAULT FALSE,
 FOREIGN KEY (id_collezione) REFERENCES collezione(id_collezione),
 FOREIGN KEY (id_carta) REFERENCES carta(id_carta)
 );
